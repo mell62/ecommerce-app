06236711d1177959a42928d92312bd387eb64416
mell62 <mridhul67@gmail.com>
2026-06-27 19:30:20 +0000

    feat: create prisma database client

---
+++ src/lib/db.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "../../src/generated/prisma";
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}

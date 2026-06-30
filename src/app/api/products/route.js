afe0383fd340ae7e9926fef45f1a724dbc9c56e7
mell62 <mridhul67@gmail.com>
2026-06-30 20:44:23 +0000

    feat: add product search API

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -6,10 +6,29 @@ export async function GET(request) {
 
   const category = searchParams.get("category");
 
+  const search = searchParams.get("search");
+
   const products = await prisma.product.findMany({
     where: {
       ...(category && {
         category: category,
+      }),
+
+      ...(search && {
+        OR: [
+          {
+            name: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+          {
+            description: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+        ],
       }),
     },
     orderBy: {

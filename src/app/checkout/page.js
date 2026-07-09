81d1c1110016d10168e52d699295e3050bfd5d9c
mell62 <mridhul67@gmail.com>
2026-07-09 20:01:13 +0000

    feat: redirect to orders after checkout

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -69,7 +69,7 @@ export default function CheckoutPage() {
 
       alert("Order placed successfully!");
 
-      window.location.href = "/";
+      window.location.href = "/orders";
     } catch (error) {
       console.error(error);
       alert(error.message);

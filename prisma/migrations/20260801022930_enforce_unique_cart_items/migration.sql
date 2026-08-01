0e4b8d9abf552e94d11e7e2d116462959e75d4f1
mell62 <mridhul67@gmail.com>
2026-08-01 23:56:45 +0530

    feat: enforce unique cart items

---
+++ prisma/migrations/20260801022930_enforce_unique_cart_items/migration.sql
@@ -0,0 +1,20 @@
+-- DropForeignKey
+ALTER TABLE "Cart" DROP CONSTRAINT "Cart_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_cartId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "CartItem" DROP CONSTRAINT "CartItem_productId_fkey";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CartItem_cartId_productId_key" ON "CartItem"("cartId", "productId");
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
